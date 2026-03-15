cf2042367812c2609270827cd01ddcf98d2fb152
Takumi Mori <51111242+takumi3488@users.noreply.github.com>
2026-03-16 03:39:14 +0900

    fix: fix &&Path type in build.rs compile_protos call

--- gqlforge-upstream-grpc/build.rs
+++ gqlforge-upstream-grpc/build.rs
@@ -15,7 +15,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     tonic_prost_build::configure()
         .file_descriptor_set_path(out_dir.join("news_descriptor.bin"))
-        .compile_protos(&[&news], &[parent])?;
+        .compile_protos(&[&news], &[&parent])?;
 
     Ok(())
 }
